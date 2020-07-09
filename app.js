8b6ae929c31db7cf2ffae5e5ca6a2a7deec648a8
olanetsoft <heedris2olubisi@gmail.com>
2020-07-09 13:03:45 +0100

    LOGIS IMPLEMENTED

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 //set the template engine ejs
 app.set('view engine', 'ejs')
 
+
 //middleware
 app.use(express.static('public'))
 
@@ -16,7 +17,9 @@ app.get('/', (req, res) => {
 
 
 //Listen on port 3000
-server = app.listen(3000)
+server = app.listen(3000, ()=>{
+    console.log("listening")
+})
 
 
 //socket.io instantiation
@@ -26,4 +29,23 @@ const io = require('socket.io')(server);
 //listen on every connection
 io.on('connection', (socket)=> {
     console.log('New user connected');
+
+    //default username
+	socket.username = "Anonymous"
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    });
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    });
+
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username})
+    });
 })
